65bdb0826a20309dd820f2c9ae9e2d63ab708df0
Gastech99 <diabygassamad16@gmail.com>
2025-08-08 11:48:17 +0000

    Third task PLP_Weekly-Code-Challenge2

---
+++ dictApp.py
@@ -0,0 +1,10 @@
+person_info = {}
+
+# Ask the user for details
+person_info["name"] = input("Enter your name: ")
+person_info["age"] = input("Enter your age: ")
+person_info["favorite_color"] = input("Enter your favorite color: ")
+
+# Print the dictionary
+print("\nHere is the information you entered:")
+print(person_info)
